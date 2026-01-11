3c18fdeb58a7b32a328df5f9b71a3c9bdd2f6d70
MarkGrayson <mgrayson15.6155000@outlook.com>
2026-01-11 21:03:18 +0000

    update location of dice images

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let scores = [0, 0];  //player scores
-let wins = [0, 0];    //player wins
+let scores = [0, 0]; //player scores
+let wins = [0, 0]; //player wins
 let currentScore = 0;
 let activePlayer = 1;
 let playing = true;
 
-const score1Txt = document.querySelector("#score--1"); 
+const score1Txt = document.querySelector("#score--1");
 const score2Txt = document.querySelector("#score--2");
-const wins1Txt = document.querySelector("#wins--1"); 
+const wins1Txt = document.querySelector("#wins--1");
 const wins2Txt = document.querySelector("#wins--2");
 const current1Txt = document.querySelector("#current--1");
 const current2Txt = document.querySelector("#current--2");
@@ -40,20 +40,19 @@ function generateDiceRoll() {
   let dice = Math.floor(Math.random() * 6) + 1;
 
   diceImg.style.display = "block";
-  diceImg.src = `/dice-${dice}.png`;
+  diceImg.src = `https://mark-grayson1.github.io/pig-game/images/dice-${dice}.png`;
   console.log(`Player ${activePlayer} rolled:
       ${dice}`);
 
   return dice;
 }
 
-function generateTotalScore()
-{
-  let totalScoreIndex = activePlayer - 1
+function generateTotalScore() {
+  let totalScoreIndex = activePlayer - 1;
   scores[totalScoreIndex] += currentScore;
 }
 
-function switchPlayer () {
+function switchPlayer() {
   currentScore = 0;
 
   document.querySelector(`#current--${activePlayer}`).textContent = "0";
@@ -61,37 +60,49 @@ function switchPlayer () {
   player1Txt.classList.toggle("player--active");
   player2Txt.classList.toggle("player--active");
   console.log(`Switch to Player ${activePlayer + 1}`);
-};
+}
 
 function checkWinner(params) {
   playing = false;
   diceImg.style.display = "none";
   wins[activePlayer - 1]++;
 
-  document.querySelector(`#wins--${activePlayer}`).textContent = `Wins: ${wins[activePlayer - 1]}`;
-  document.querySelector(`.player--${activePlayer}`).classList.add  ("player--winner");
-  document.querySelector(`.player--${activePlayer}`).classList.remove   ("player--active");
+  document.querySelector(`#wins--${activePlayer}`).textContent = `Wins: ${
+    wins[activePlayer - 1]
+  }`;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add("player--winner");
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove("player--active");
 
-  console.log(`Player ${activePlayer} wins the game! Total Wins: ${wins[activePlayer - 1]}`);
-  };
+  console.log(
+    `Player ${activePlayer} wins the game! Total Wins: ${
+      wins[activePlayer - 1]
+    }`
+  );
+}
 
-rollBtn.addEventListener("click", ()=> {
+rollBtn.addEventListener("click", () => {
   if (playing) {
     let dice = generateDiceRoll();
 
-    if(dice !== 1) {
+    if (dice !== 1) {
       currentScore += dice;
-      document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
     } else {
       switchPlayer();
-      }
+    }
   }
 });
 
-holdBtn.addEventListener("click", ()=> {
+holdBtn.addEventListener("click", () => {
   if (playing) {
     generateTotalScore();
-    document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer - 1];
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer - 1];
 
     console.log(`Player ${activePlayer - 1} holds:
       Total Score: ${scores[activePlayer - 1]}`);
